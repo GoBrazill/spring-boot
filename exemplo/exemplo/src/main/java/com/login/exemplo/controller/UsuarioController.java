475d2c25f23b17ec08f5114b1db668c43172786e
GoBrazill <vinicius.daraujos26@gmail.com>
2025-10-23 11:16:40 -0300

    Feat: add get and delete methods

--- exemplo/exemplo/src/main/java/com/login/exemplo/controller/UsuarioController.java
+++ exemplo/exemplo/src/main/java/com/login/exemplo/controller/UsuarioController.java
@@ -1,8 +1,14 @@
 package com.login.exemplo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +44,29 @@ public class UsuarioController {
 			}
 		}
 		
+	}
+	
+	@GetMapping(value = "listar/usuarios")
+	public List<Usuario> listUsuarios(){
+//		List<Usuario> usuarios = usuarioRepository.findAll();
+		
+		return usuarioRepository.findAll();
+	}
+	
+	@GetMapping(value = "listar/usuario/{id}")
+	public Optional<Usuario> usuarioPorId(@PathVariable int id){
+		
+		return usuarioRepository.findById(id);
+	}
+	
+	@DeleteMapping(value = "delete/usuario/{id}")
+	public void deletePorId(@PathVariable int id){
+		if (usuarioRepository.existsById(id) == true){
+			
+ 			System.out.println("Usuário deletado com sucesso0");
+		} else {
+			
+			System.out.println("Usuário não encontrado");
+		}
 	}
 }
